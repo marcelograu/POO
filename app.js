76450f157eb0787b32d4ad90a826668a7ef53eff
Instrutor GTBetim <instrutor@GTBETIM.local>
2024-08-02 15:43:19 -0300

    updating

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 
-const ref =  document.querySelector('.foguete')
 
-
-
-const fogueteObj = {
+const carro = {
+    modelo: "fusca",
+    fabricante: "VW",
+    ano: 1968,
+    kmRodado: 400,
+    cor: preto,
     
-    tamanho: 'normal',
-   
-    decolar: function() {
-        
-        ref.classList.add('decolar')
-        console.log(ref)
-    },
-    rodar: function(){
-        ref.classList.add('rodar')
-    }
-   
 }
+
+
+const cachorro =  {
+    //atributos
+    raca: "vira-lata rebaixado",
+    cor: 'branco',
+    sexo: 'fêmea',
+    tamanho: 'pequeno',
+    peso: '14kg',
+    idade: 3,
+    nome: "Braquinha",
+    //comportamentos
+    seApresentar: function () {
+    console.log( "Oi meu nome é ", this.nome, "minha raça é ", this.raca)
+    },
+    latir: function() {
+        console.log("AHAHUAHAUHAHA AHAHA AU AUAU AUA AUA AU AU ")
+    },
+    rosnar: function(){
+        console.log('GRRRRRRRRRRRRRR!!!')
+    },
+    
+}
+
+console.log(   
+
+  cachorro.cor,
+
+
+)
